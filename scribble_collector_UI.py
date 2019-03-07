8d1a07ab8d5a9eee766d0e008f9bd27fb550c75c
Qian Shenhan <qianshenhan@hnu.edu.cn>
2019-03-07 20:05:31 +0800

    Adjust window default size

--- scribble_collector_UI.py
+++ scribble_collector_UI.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(635, 505)
+        Form.resize(485, 396)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
